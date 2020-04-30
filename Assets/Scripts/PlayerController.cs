e410777e3f38517670f5845d5d72f4e648d7f849
Benjamin Daniel <postanbenny@gmail.com>
2020-04-30 11:40:55 +0200

    More comments and forwardDirection refactor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,21 +18,27 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         // looks in the (current) gameObject for component of type Rigidbody
+        // gameObject.GetComponent<Rigidbody>() would work as well
         playerRb = GetComponent<Rigidbody>();
     }
 
+    // Update is called once per frame
     void Update()
     {
         //powerupIndicator.transform.position = transform.position + new Vector3(0f, -0.5f, 0f);
     }
 
+    // FixedUpdate is called on a fixed physics loop
     void FixedUpdate()
     {
         // gets the vertical user input from joystick/keyboard (w/s or up/down)
         // as a float between -1 and 1
         float forwardInput = Input.GetAxis("Vertical");
+        // gets the normalized forward direction of the focal point aka camera
+        // normalized means values of the vector range between -1 and 1
+        Vector3 forwardDirection = focalPoint.transform.forward.normalized;
         // adds force to the player in the direction the camera is facing
-        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        playerRb.AddForce(forwardDirection * forwardInput * speed);
     }
 
     void OnTriggerEnter(Collider other)
--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
-    public float rotationSpeed = 50.0f;
+    // public -> visible in inspector and other scripts
+    // private -> not visible in inspector and other scripts
+    public float rotationSpeed = 50.0f; // floating point numbers require 'f' at the end
 
     // Update is called once per frame
     void Update()
     {
+        // gets the horizontal user input from
+        // joystick/keyboard (a/d or left/right) as a float between -1 and 1
         float horizontalInput = Input.GetAxis("Horizontal");
+        // rotates the focal point and all of it's attached children (main camera)
+        // requires an axis to be rotated around and a rotation angle
         transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
