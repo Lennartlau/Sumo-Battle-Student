8799f97909dba7250c319c5991f4dd11bf2f709b
Lennartlau <64399535+Lennartlau@users.noreply.github.com>
2020-05-03 22:07:00 +0200

    first lecture changes

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float RotationSpeed = 50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * RotationSpeed * Time.deltaTime);
+    }
+}
